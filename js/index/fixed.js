40c90f8ed67bd0b0f70b1eceaa6255426e7adc59
iamkepo <christamour.kakpo@gmail.com>
2022-08-06 15:21:37 +0200

    validation essai

---
+++ js/index/fixed.js
@@ -0,0 +1,12 @@
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+
+window.addEventListener('scroll', e=> {
+  
+  if (window.scrollY > header.clientHeight) {
+    nav.classList.add("fixed")
+  } else {
+    nav.classList.remove("fixed")
+  }
+
+})
