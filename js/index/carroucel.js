ccb0a1fc5640a86c9437e3b49a917e469d396c01
iamkepo <christamour.kakpo@gmail.com>
2022-08-07 20:45:48 +0200

    stable

--- js/index/carroucel.js
+++ js/index/carroucel.js
@@ -1,18 +1,32 @@
 let section1 = document.querySelector(".section1");
 let levels = document.querySelectorAll(".level");
+
 let slideIndex = 0;
+let timer = null;
+
 showSlides();
 
-function showSlides() {
-  
-  for (let i = 0; i < slideIndex; i++) {
-    levels[i].style.backgroundColor = "transparent";
-    
+for (let i = 0; i < levels.length; i++) {
+  levels[i].onclick = () => {
+    slideIndex = i;
+    rested(levels.length);
+    clearTimeout(timer)
+    showSlides();
   }
+}
+
+function showSlides() {
+  rested(slideIndex);
   if (slideIndex > 2) {slideIndex = 0}
   section1.style.animation = "fade 2s";
   section1.style.backgroundImage = "url('./img/index/car"+slideIndex+".svg')";
   levels[slideIndex].style.backgroundColor = "#FFF";
   slideIndex++;
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  timer = setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+
+function rested(length) {
+  for (let i = 0; i < length; i++) {
+    levels[i].style.backgroundColor = "transparent";
+  }
 }
--- js/index/fixed.js
+++ js/index/fixed.js
@@ -1,12 +1,18 @@
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
+const backTop = document.querySelector(".section5-btn");
+
 
 window.addEventListener('scroll', e=> {
   
   if (window.scrollY > header.clientHeight) {
     nav.classList.add("fixed")
   } else {
-    nav.classList.remove("fixed")
+    nav.classList.remove("fixed");
   }
 
 })
+
+backTop.onclick = function (e) {
+  window.scrollTo(0, 0)
+}
